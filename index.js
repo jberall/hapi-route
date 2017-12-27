7c4730fb9bbcc2b475c9c26b57abe07b77680d40
Jonathan Berall <jberall@gmail.com>
2017-12-27 14:07:34 -0500

    added another route and changed version

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 'use strict';
 
 exports.register = function (server, options, next) {
-    server.route({
-        method: 'GET',
-        path: '/test-github',
-        handler: function (request, reply) {
-            reply('test-github passed ');
-        }
-    });
+    server.route([
+        {
+            method: 'GET',
+            path: '/test-github',
+            handler: function (request, reply) {
+                reply('test-github passed ');
+            }
+        },
+        {
+            method: 'GET',
+            path: '/test-github2',
+            handler: function (request, reply) {
+                reply('test-github2 passed ');
+            }
+        }        
+    ]);
     next();
 };
 
